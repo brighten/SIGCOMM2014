5feb359747247df6adc14abd19ff85cf088291df
Xuefeng-Zhu <xzhu15@illinois.edu>
2014-07-23 23:25:17 -0400

    uncomment_day_filter

--- web/include/program/sigcomm.php
+++ web/include/program/sigcomm.php
@@ -1,4 +1,4 @@
-<!--
+
 <div id="prog_ctrl" data-role="navbar">
 	<ul>
 		<li><a href="#">Tuesday</a></li>
@@ -7,7 +7,7 @@
 		<li><a href="#" class="ui-btn-active ui-state-persist">All</a></li>
 	</ul>
 </div>
--->
+
 
 <ul id="proglist" data-role="listview" data-filter="true" data-inset="true" data-theme="d" data-dividertheme="a">
 <!-- <ul id="proglist" data-role="listview" data-filter="true" data-inset="true" data-theme="d" data-content-theme="d" class="tprog"> -->
